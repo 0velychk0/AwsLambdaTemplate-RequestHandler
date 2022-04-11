e675cc6831243d48d0bebc30f22faa9de8ef93e2
Oleksandr_Velychko2 <Alex.Velichko@gmail.com>
2022-04-11 16:43:46 +0300

    Add Shadow plugin to Maven and Gradle builds to support AWS Lambda

--- src/main/java/com/ovelychko/MainApplication.java
+++ src/main/java/com/ovelychko/MainApplication.java
@@ -3,28 +3,27 @@ package com.ovelychko;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
 
-import java.util.Map;
-
-public class MainApplication implements RequestHandler<Map<String, String>, String> {
+public class MainApplication implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
 
     public MainApplication() {
-        System.out.println("MainApplication created");
+        System.out.println("MainApplication created 1");
     }
 
     @Override
-    public String handleRequest(Map<String, String> stringStringMap, Context context) {
+    public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent event, Context context) {
         LambdaLogger logger = context.getLogger();
 
-        String response = "200 OK";
-
         // log execution details
+        logger.log("EVENT: " + event);
         logger.log("ENVIRONMENT VARIABLES: " + System.getenv());
         logger.log("CONTEXT: " + context);
 
-        // process event
-        logger.log("EVENT: " + stringStringMap.toString());
-        logger.log("EVENT TYPE: " + stringStringMap.getClass());
-        return response;
+        return new APIGatewayProxyResponseEvent()
+                .withStatusCode(200)
+                .withBody("code: 200 Done")
+                .withIsBase64Encoded(false);
     }
 }
